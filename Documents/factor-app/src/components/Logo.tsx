3c8c27f02fd38c320ac2bfafa09575f33f00cdf0
Sabih-sourcee <darahunting133@gmail.com>
2026-07-15 17:31:27 +0500

    Remove factor-app, src, config, and requirements from new-folder

--- Documents/factor-app/src/components/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export const Logo = ({ className = 'w-8 h-8', invert = false }: { className?: string; invert?: boolean }) => (
-  <img
-    src="/image_1.png"
-    alt="Factor LED"
-    className={`${className} object-contain${invert ? ' invert brightness-200' : ''}`}
-    referrerPolicy="no-referrer"
-  />
-);
--- Documents/factor-app/src/components/SkeletonCard.tsx
+++
@@ -1,9 +0,0 @@
-export const SkeletonCard = () => (
-  <div className="flex flex-col gap-4">
-    <div className="aspect-[4/3] rounded-3xl skeleton" />
-    <div className="px-1 flex flex-col gap-2">
-      <div className="h-5 w-3/4 rounded-full skeleton" />
-      <div className="h-3 w-1/2 rounded-full skeleton" />
-    </div>
-  </div>
-);
